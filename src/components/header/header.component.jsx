61582a0e2164dd6bbe1cc8e30ce0fec56e115357
Yukthshree <yukthinaik66@gmail.com>
2020-05-19 07:01:12 +0530

    finishing sign in handleSubmit method

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -15,13 +15,16 @@ const Header = ({ currentUser }) => (
             </Link>
             <Link className='option' to="/shop">
                 CONTACT
-            </Link>
-            {
-                currentUser ?
-                    <div className='option' onClick={() => auth.signOut()}></div>
-                    :
-                    <Link className='option' to='/signin'></Link>
-            }
+                </Link>
+            {currentUser ? (
+                <div className='option' onClick={() => auth.signOut()}>
+                    SIGN OUT
+        </div>
+            ) : (
+                    <Link className='option' to='/signin'>
+                        SIGN IN
+        </Link>
+                )}
         </div>
     </div>
 )
