df1c5164f891620278783dd2a3458326dc1736f9
Yukthshree <yukthinaik66@gmail.com>
2020-05-20 07:52:29 +0530

    styling addtoCart button + update shop data with new data + updated cart utils to group items in cart reducer

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -3,4 +3,9 @@ import CartActionTypes from './cart.types';
 export const toggleCartHidden = cart => ({
     type: CartActionTypes.TOGGLE_CART_HIDDEN,
     payload: cart,
-})
+});
+
+export const addItem = item => ({
+    type: CartActionTypes.ADD_ITEM,
+    payload: item,
+});
