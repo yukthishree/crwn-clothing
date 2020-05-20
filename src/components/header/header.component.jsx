83b7dfac687c69bcf112912f5401fb1bbd81169e
Yukthshree <yukthinaik66@gmail.com>
2020-05-20 06:47:32 +0530

    added cart icon + card Dropdown + card reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
 import { ReactComponent as Logo } from '../../assets/original.svg';
+
 import './header.style.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
-        <Link className='logo-container' to="/">
+        <Link className='logo-container' to='/'>
             <Logo className='logo' />
         </Link>
         <div className='options'>
-            <Link className='option' to="/shop">
+            <Link className='option' to='/shop'>
                 SHOP
-            </Link>
-            <Link className='option' to="/shop">
+      </Link>
+            <Link className='option' to='/shop'>
                 CONTACT
-                </Link>
+      </Link>
             {currentUser ? (
                 <div className='option' onClick={() => auth.signOut()}>
                     SIGN OUT
@@ -26,12 +31,15 @@ const Header = ({ currentUser }) => (
                         SIGN IN
         </Link>
                 )}
+            <CartIcon />
         </div>
+        {hidden ? null : <CartDropdown />}
     </div>
-)
+);
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden
 });
 
-export default connect(mapStateToProps, null)(Header);
+export default connect(mapStateToProps)(Header);
---
+++ src/redux/cart/cart.actions.js
@@ -0,0 +1,6 @@
+import CartActionTypes from './cart.types';
+
+export const toggleCartHidden = cart => ({
+    type: CartActionTypes.TOGGLE_CART_HIDDEN,
+    payload: cart,
+})
